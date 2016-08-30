de1ab195aeef9253c0aa0bea55d03ef0b79f962b
Ion Gorincioi <gorincioii@yahoo.com>
2016-08-30 16:45:54 -0700

    Issues with infinite scroll

--- public/javascript/reddit.js
+++ public/javascript/reddit.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
 //by default:
 
-
+var glob;
   function reddit(){
     $.getJSON("https://www.reddit.com/r/all/.json?limit=100&after=t3_10omtd/ ", function(r) {
       console.log("hai", r.data.children);
@@ -15,14 +15,8 @@ $(document).ready(function(){
   }
   reddit();
 
-  $("button.add").on("click", function(event){
-    event.preventDefault();
-    $("#main").empty();
-
-      var k = $("input").val();
-    $("#subr1").append("<button value='" + k + "' type='click' id='subr' class='btn btn-xs' aria-label='Left Align'>" + k + "</button> ");
-      console.log("input", k);
-    $.getJSON("https://www.reddit.com/r/" + k + ".json?limit=100&after=t3_10omtd/ ", function(r) {
+  function reddit1(k){
+      $.getJSON("https://www.reddit.com/r/" + k + ".json?limit=100&after=t3_10omtd/ ", function(r) {
       console.log("hai1", r.data.children);
       r.data.children.forEach(function(i){
         var result = "<div class='" + k +"'><p class=''>R/"+ i.data.subreddit + "  |  NAME: "+"<a href='" +i.data.url +" ' target='_blank'><span class='name'>"+ i.data.title +"</span></a>" + "  |  SCORE: " + i.data.score +"</p><hr></div>";
@@ -32,13 +26,34 @@ $(document).ready(function(){
       
       })
     });
+  }
+
+  $("button.add").on("click", function(event){
+    event.preventDefault();
+    $("#main").empty();
+
+    var k = $("input").val();
+    $("#subr1").append("<button value='" + k + "' type='click' id='subr' class='btn btn-xs' aria-label='Left Align'>" + k + "</button> ");
+      console.log("input", k);
+      reddit1(k);
+  
 
   })
 
   $(document).on("click", "button#subr", function(){
     alert("REMOVE THE SUBREDDIT!");
     var myClass = "." + $(this).html();
+    glob = $(this).html();
     $(myClass).remove();
     $(this).remove();
   })
+
+  // $(window).scroll(function() {
+  //   var y = $("div#post>div").className;
+  //   console.log("yyyy", y);
+  //   reddit1(y);
+  //   $("#post").apend(result1);
+
+  // });
+
 })
